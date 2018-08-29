28569a1e9d5a820a499d42b1f6451188de14ee4f
Robin Pike <Robin.Pike@learningtrust.co.uk>
2018-08-29 15:16:55 +0100

    Create initial Django app

--- BasicProject/BasicProject/urls.py
+++ BasicProject/BasicProject/urls.py
@@ -3,8 +3,9 @@ Definition of urls for BasicProject.
 """
 
 from datetime import datetime
-from django.conf.urls import url
+from django.conf.urls import include, url
 import django.contrib.auth.views
+import HelloDjangoApp.views
 
 import app.forms
 import app.views
@@ -14,33 +15,8 @@ import app.views
 # from django.contrib import admin
 # admin.autodiscover()
 
+#Djnago processes patterns in the order in which they appear in the array
 urlpatterns = [
-    # Examples:
-    url(r'^$', app.views.home, name='home'),
-    url(r'^contact$', app.views.contact, name='contact'),
-    url(r'^about$', app.views.about, name='about'),
-    url(r'^login/$',
-        django.contrib.auth.views.login,
-        {
-            'template_name': 'app/login.html',
-            'authentication_form': app.forms.BootstrapAuthenticationForm,
-            'extra_context':
-            {
-                'title': 'Log in',
-                'year': datetime.now().year,
-            }
-        },
-        name='login'),
-    url(r'^logout$',
-        django.contrib.auth.views.logout,
-        {
-            'next_page': '/',
-        },
-        name='logout'),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', HelloDjangoApp.views.index, name='index'),
+    url(r'^$', app.views.home, name='home')
 ]
