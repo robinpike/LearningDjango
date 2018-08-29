863353d891a04f5c8c7793e2ae1df60907d58e1b
Robin Pike <Robin.Pike@learningtrust.co.uk>
2018-08-29 23:33:01 +0100

    Create a Django app

--- BasicProject/HelloDjangoApp/views.py
+++ BasicProject/HelloDjangoApp/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 
 def index(request):
-    return HttpResponse("Hello, Django!")
+    now = datetime.now()
+
+    return render(request,
+                  "HelloDjangoApp/index.html",
+                  {
+                      'title': "Hello Django",
+                      'message': "Hello Django!",
+                      'content': " on " + now.strftime("%A, %d %B, %Y at %X")       
+                  }
+             )
+    
+    #html_content = "<html><head><title>Hello, Django</title></head><body><strong>Hello, Django!</strong> on "
+    #html_content += now.strftime("%A, %d %B, %Y at %X")
+    #html_content += "</body></html>"
+    
+    #return HttpResponse(html_content)
